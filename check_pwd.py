0aa5afd4d9f9237050754bfb48843169fc6727d7
Robert Smith <smithro8@oregonstate.edu>
2024-02-09 15:22:37 -0800

    made check_pwd return False when the length is equal to or greater than21, it passed. Refactored to one line.

--- check_pwd.py
+++ check_pwd.py
@@ -1,8 +1,5 @@
 def check_pwd(password):
-    if len(password) <= 7:
-        return False
-
-    if len(password) == 21:
+    if len(password) <= 7 or len(password) >= 21:
         return False
 
     return True
