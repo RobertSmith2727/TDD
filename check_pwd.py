cdf98d87985e1807a6b3b792f240a271a051a94e
Robert Smith <smithro8@oregonstate.edu>
2024-02-09 16:08:53 -0800

    made check_pwd return False when pwd does not contain lowercase, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -2,7 +2,7 @@ def check_pwd(password):
     if len(password) <= 7 or len(password) >= 21:
         return False
 
-    if password.isupper():
+    if not any(element.islower() for element in password):
         return False
 
     return True
