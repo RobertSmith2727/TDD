1437cc9bcdbec4bbdb8048dc5f261482fc176d57
Robert Smith <smithro8@oregonstate.edu>
2024-02-12 16:52:18 -0800

    made check_pwd return False when pwd does not contain symbol , it passed

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,5 @@
 def check_pwd(password):
+
     if len(password) <= 7 or len(password) >= 21:
         return False
 
@@ -9,6 +10,12 @@ def check_pwd(password):
         return False
 
     if not any(element.isnumeric() for element in password):
+        return False
+
+    symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_",
+               "+", "-", "="]
+
+    if not any(element in symbols for element in password):
         return False
 
     return True
